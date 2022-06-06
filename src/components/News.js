11107026963555723419d8b6ae7e84ee7e9f1a71
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-06-06 12:44:43 +0530

    Create News.js

---
+++ src/components/News.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+const News = () => {
+  const [lang, setLang] = useState("en");
+  const [articles, setArticles] = useState([]);
+  const API__KEY = "7a60264c645891802f5b781be1da6a33";
+
+  useEffect(() => {
+    fetch(
+      `https://gnews.io/api/v4/search?q=example&token=${API__KEY}&lang=${lang}`
+    )
+      .then((res) => res.json())
+      .then((response) => setArticles(response.articles));
+  }, [lang]);
+  return (
+    <div>
+      <h2>News-Info</h2>
+      <button
+        data-testid="lang-en"
+        onClick={(e) => setLang(e.target.getAttribute("data-testid").slice(5))}
+      >
+        English
+      </button>
+      <button
+        data-testid="lang-hi"
+        onClick={(e) => setLang(e.target.getAttribute("data-testid").slice(5))}
+      >
+        Hindi
+      </button>
+      {articles.length > 0 ? (
+        articles.map((item, index) => (
+          <div key={index}>
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+          </div>
+        ))
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+};
+
+export default News;
