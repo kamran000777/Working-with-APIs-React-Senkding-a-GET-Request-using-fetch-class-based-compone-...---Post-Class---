6e6618078fdcebbfece4dbd810b06aabd86c7136
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-06-06 12:43:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component, useState} from "react";
+import Weather from "./Weather";
+import News from "./News";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
+    <>
+    <Weather />
+      <News />
+    </>
   )
 }
 
